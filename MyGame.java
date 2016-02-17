ef4b124f6e969403485f90d3d7b79b757cd65e60
InsertGitHubUsernameHere <t_le@uncg.edu>
2016-02-17 00:04:59 -0500

    Updated MyGame.java

--- MyGame.java
+++ MyGame.java
@@ -23,8 +23,14 @@ public class MyGame {
                 myGame.printBoard();
 
                 System.out.println("Choose a column:");
-                System.out.print("> Player " + myGame.getTurn() + " turn!\t");
-
+                if (myGame.getTurn() == 'R') {
+                    System.out.println(">Player One(R) turn!");
+                } else if(myGame.getTurn() == 'Y'){
+                    System.out.print("> Player Two(Y) turn!\t");
+                }
+                /*while((myGame.isColFull(input.nextInt()))){
+                    System.out.println("Please choose a different column!");
+                }*/
                 do {
                     try {
                         Exception = false;
@@ -33,6 +39,7 @@ public class MyGame {
                     } catch (ArrayIndexOutOfBoundsException c) {
                         Exception = true;
                         System.out.print("Please insert a valid number! ");
+                        
                     }
 
                 } while (Exception == true);
@@ -60,8 +67,7 @@ public class MyGame {
 
                     Repeat = true;
 
-
-       } else if (Loop.equalsIgnoreCase("n")) {
+                } else if (Loop.equalsIgnoreCase("n")) {
                     System.out.println("Thank you for playing Connect Four" + ", BYE :D ");
                 } else {
                     System.out.println("Please enter Y or N!");
